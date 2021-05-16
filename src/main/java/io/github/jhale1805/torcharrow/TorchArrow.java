72733f2fb665992368c9b42a699da49113076022
Joseph Hale <j.hale1805@gmail.com>
2021-05-15 20:05:05 -0700

    Create base plugin structure

---
+++ src/main/java/io/github/jhale1805/torcharrow/TorchArrow.java
@@ -0,0 +1,10 @@
+package io.github.jhale1805.torcharrow;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TorchArrow extends JavaPlugin {
+
+    public static boolean someLibraryMethod() {
+        return true;
+    }
+}
