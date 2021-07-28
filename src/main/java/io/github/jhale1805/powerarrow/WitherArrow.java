14630d41178180e18a9988c122dbde09f0a0283d
Daniel <dphale20x2@gmail.com>
2021-07-28 09:21:43 -0700

    Added recipe, name, just need to finish the onThisProjectileShot method

--- src/main/java/io/github/jhale1805/powerarrow/WitherArrow.java
+++ src/main/java/io/github/jhale1805/powerarrow/WitherArrow.java
@@ -1,30 +1,39 @@
 package io.github.jhale1805.powerarrow;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.ShapelessRecipe;
+
 
 public class WitherArrow extends PowerArrow {
 
     @Override
     public String getName() {
-        // TODO Auto-generated method stub
-        return null;
+        return "wither_arrow";
     }
 
     @Override
     public String[] getUsageInstructions() {
-        // TODO Auto-generated method stub
-        return null;
+        return new String[]{"Launches a wither skull", "instead of an arrow"};
     }
 
     @Override
     public Recipe getRecipe() {
-        // TODO Auto-generated method stub
-        return null;
+        ShapelessRecipe recipe = new ShapelessRecipe(
+                this.getRecipeKey(),
+                new TeleportArrow()
+        );
+        recipe.addIngredient(Material.WITHER_SKELETON_SKULL)
+                .addIngredient(Material.ARROW);
+        return recipe;
+
     }
 
     @Override
     public void onThisProjectileShot(EntityShootBowEvent event) {
+        Player shooter = (Player) event.getEntity().getShooter();
         // TODO Get the entity that shot the arrow from the event.
         // TODO Launch a wither skull from that entity.
     }
